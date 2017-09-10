6c69cf5566dadfb729241726a68d161f174f000c
Chris M <webmaster@magnaws.com>
2017-09-10 12:12:56 -0400

    3.5.0 release.

--- README.md
+++ README.md
@@ -53,6 +53,10 @@ There are a number of sample files included with the installation. These are not
 
 CHANGELIST
 ----------
+***Version 3.5.0* ** *- September 10, 2017*
+
+- Added the capability to have different localizations apply to different settings.
+
 ***Version 3.4.7* ** *- June 17, 2017*
 
 - Added a workaround for nonstandard SSL certs.
--- config-bmlt-basic.inc.php
+++ config-bmlt-basic.inc.php
@@ -3,7 +3,7 @@
 *   \file   config-bmlt-basic.inc.php                                                       *
 *                                                                                           *
 *   \brief  This file contains the basic configuration directives for the standalone client.*
-*   \version 3.4.7                                                                          *
+*   \version 3.5.0                                                                          *
 *                                                                                           *
 *   This file comes with the sample set for the Greater New York Region BMLT Server, which  *
 *   is run by the same people that designed the BMLT, so it can be considered "Home Field." *
@@ -130,10 +130,11 @@ $bmlt_basic_configuration[$bmlt_basic_configuration_index] = array (    // Leave
     /** These are optional. Most folks will leave them at these values. They are used by the '[[BMLT]]', '[[BMLT_MOBILE]]' and '[[BMLT_MAP]]' shortcodes.   */
     /********************************************************************************************************************************************************/
     
-    'grace_period'                  =>          15, /* How many minutes are allowed to go by before a meeting is considered "too late."                     */
-    'time_offset'                   =>          0,  /* Generally left at 0 hours. If the server has a different time offset from this, indicate it here.    */
-    'military_time'                 =>          0,  /* This is 1 if you want military (24-hour) time.                                                       */
-    'startWeekday'                  =>          1,  /* This is 1-7, as the start weekday of each week (1 = Sunday, 7 = Saturday)                            */
+    'grace_period'                  =>          15,     /* How many minutes are allowed to go by before a meeting is considered "too late."                     */
+    'time_offset'                   =>          0,      /* Generally left at 0 hours. If the server has a different time offset from this, indicate it here.    */
+    'military_time'                 =>          0,      /* This is 1 if you want military (24-hour) time.                                                       */
+    'startWeekday'                  =>          1,      /* This is 1-7, as the start weekday of each week (1 = Sunday, 7 = Saturday)                            */
+    'lang'                          =>          'en',   /* This is the language code for individual localization.                                               */
     
 /*************************************************************************************************************************************************************
 *^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^*
