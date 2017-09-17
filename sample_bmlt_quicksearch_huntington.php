523a88f6baf7969c2f23975550547fca193e6e06
Chris M <webmaster@magnaws.com>
2017-09-17 19:14:31 -0400

    Updated the base class, and tweaked the page title for the Huntington example.

--- sample_bmlt_quicksearch_huntington.php
+++ sample_bmlt_quicksearch_huntington.php
@@ -11,7 +11,7 @@ require_once ( dirname ( __FILE__ ).'/bmlt-basic/bmlt_basic.class.php' );
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
     <head>
         <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-        <title>All New York Database Meetings As A Fast, Simple Search, with the Popup Listing States</title>
+        <title>All New York Database Meetings As A Fast, Simple Search, with the Popup Listing the Township of Huntington, Long Island, NY</title>
         <?php $basic_bmlt_object->output_head('[[BMLT_QUICKSEARCH(location_municipality=HuNtInGtoN,greenlawn,DIX HILLS,Melville,Huntington Station,south HUNTINGTON,Ellwood,Northport,east northport)]]'); ?>
     </head>
     <body>
